ec3812299406739929ac4e0598494b5ad8382444
Niccolò <niccolo.sorrenti@gmail.com>
2026-03-19 13:46:36 +0100

    es1, fatto

---
+++ src/main/java/niccolosorrenti/entities/Customer.java
@@ -0,0 +1,22 @@
+package niccolosorrenti.entities;
+
+public class Customer {
+    private Long id;
+    private String name;
+    private int tier;
+
+    public Customer(long id, String name, int tier) {
+        this.id = id;
+        this.name = name;
+        this.tier = tier;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", tier=" + tier +
+                '}';
+    }
+}
---
+++ src/main/java/niccolosorrenti/entities/Order.java
@@ -0,0 +1,62 @@
+package niccolosorrenti.entities;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Order {
+    protected List<Product> products;
+    private long id;
+    private String status;
+    private LocalDate orderDate;
+    private LocalDate deliveryDate;
+    private Customer customer;
+
+    public Order(long id, String status, LocalDate orderDate, LocalDate deliveryDate, List<Product> products, Customer customer) {
+        this.id = id;
+        this.status = status;
+        this.orderDate = orderDate;
+        this.deliveryDate = deliveryDate;
+        this.products = products;
+        this.customer = customer;
+    }
+
+//    public static int calculateTotal() {
+//
+//    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public LocalDate getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "products=" + products +
+                ", id=" + id +
+                ", status='" + status + '\'' +
+                ", orderDate=" + orderDate +
+                ", deliveryDate=" + deliveryDate +
+                ", customer=" + customer +
+                '}';
+    }
+}
